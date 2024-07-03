031ea492c0eae19b8a7457a365bd6c692c75bd29
CARLOS MANUEL ESPINOZA MENDOZA <2020371019@gmail.com>
2024-07-02 23:41:33 -0600

    (feat):Creación_Carousel

--- src/components/pages/Carousel/Carusel.jsx
+++ src/components/pages/Carousel/Carusel.jsx
@@ -1,26 +1,47 @@
 import React from 'react';
 import { Carousel } from 'antd';
+import an from '../../../assets/an.png';
+import bn from '../../../assets/bn.png';
+import cn from '../../../assets/cn.png';
+import dn from '../../../assets/dn.png';
+
 const contentStyle = {
-  height: '160px',
-  color: '#fff',
-  lineHeight: '160px',
+  height: '260px',
   textAlign: 'center',
   background: '#364d79',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
 };
+
+const imageStyle = {
+  maxWidth: '100%',
+  maxHeight: '100%',
+};
+
 const Carrousel = () => (
   <Carousel autoplay>
     <div>
-      <h3 style={contentStyle}>1</h3>
+      <div style={contentStyle}>
+        <img src={an} alt="Slide 1" style={imageStyle} />
+      </div>
     </div>
     <div>
-      <h3 style={contentStyle}>2</h3>
+      <div style={contentStyle}>
+        <img src={bn} alt="Slide 2" style={imageStyle} />
+      </div>
     </div>
     <div>
-      <h3 style={contentStyle}>3</h3>
+      <div style={contentStyle}>
+        <img src={cn} alt="Slide 3" style={imageStyle} />
+      </div>
     </div>
     <div>
-      <h3 style={contentStyle}>4</h3>
+      <div style={contentStyle}>
+        <img src={dn} alt="Slide 4" style={imageStyle} />
+      </div>
     </div>
   </Carousel>
 );
+
 export default Carrousel;
