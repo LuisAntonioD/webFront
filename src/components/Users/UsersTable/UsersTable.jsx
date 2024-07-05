9605761951ea4e996daf9f834785451ac75ed90d
AuraRenata812 <becaria.aura@mideservices.com>
2024-07-04 18:16:03 -0600

    funcion de eliminar usaurio en tabla added

--- src/components/Users/UsersTable/UsersTable.jsx
+++ src/components/Users/UsersTable/UsersTable.jsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState, useContext } from 'react';
 import axios from 'axios';
-import { Button} from 'antd';
-import { RiAddLine } from 'react-icons/ri';
+import { Button, Modal, notification } from 'antd';
+import { RiAddLine, RiDeleteBin6Line } from 'react-icons/ri';
 import { ENV } from '../../../utils/constants';
-import authService from '../../../services/admisiones';
+import usersService from '../../../services/users';
 import { AuthContext } from '../../context/AuthContext';
 import './UsersTable.css';
 
@@ -13,10 +13,10 @@ const UsersTable = () => {
     const { user, token } = useContext(AuthContext);
 
     useEffect(() => {
-        fetchProducts();
+        fetchUsers();
     }, []);
 
-    const fetchProducts = async () => {
+    const fetchUsers = async () => {
         try {
             const response = await axios.get(`${ENV.API_URL}/${ENV.ENDPOINTS.USER}`);
             if (Array.isArray(response.data)) {
@@ -30,6 +30,36 @@ const UsersTable = () => {
         }
     };
 
+    const deleteUser = async (id) => {
+        try {
+            await usersService.deleteUser(id, token);
+            fetchUsers();
+            showDeleteNotification();
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    const showDeleteNotification = () => {
+        notification.success({
+            message: 'Usuario Eliminado',
+            description: 'Usuario eliminado correctamente.',
+        });
+    };
+
+    const confirmDeleteUser = (id) => {
+        Modal.confirm({
+            title: 'Confirmar Eliminación',
+            content: '¿Estás seguro de que deseas eliminar a este usuario?',
+            okText: 'Eliminar',
+            okType: 'danger',
+            cancelText: 'Cancelar',
+            onOk() {
+                deleteUser(id);
+            },
+        });
+    };
+
     const formatDate = (dateString) => {
         const options = { year: 'numeric', month: 'numeric', day: 'numeric' };
         return new Date(dateString).toLocaleDateString(undefined, options);
@@ -41,15 +71,15 @@ const UsersTable = () => {
 
     return (
         <div className="users-table-page">
-             <div className="buttons-container">
-                    <Button
-                        className="add-button"
-                        type="primary"
-                        icon={<RiAddLine />}
-                    >
-                        Agregar Usuario
-                    </Button>
-                </div>
+            <div className="buttons-container">
+                <Button
+                    className="add-button"
+                    type="primary"
+                    icon={<RiAddLine />}
+                >
+                    Agregar Usuario
+                </Button>
+            </div>
             <div className="table-container table-wrapper">
                 <table className="formato-tabla">
                     <thead>
@@ -61,21 +91,22 @@ const UsersTable = () => {
                         </tr>
                     </thead>
                     <tbody>
-                        {users.map((users) => (
-                            <tr key={users._id}>
-                                <td>{users.username}</td>
-                                <td>{users.email}</td>
-                                <td>{formatDate(users.createdAt)}</td>
+                        {users.map((user) => (
+                            <tr key={user._id}>
+                                <td>{user.username}</td>
+                                <td>{user.email}</td>
+                                <td>{formatDate(user.createdAt)}</td>
                                 <td>
-                                            <Button
-                                             className="action-button ant-btn-danger"
-                                             >
-                                                Eliminar
-                                            </Button>
-                                            <Button
-                                              className="action-button ant-btn-success">
-                                                Editar
-                                            </Button>   
+                                    <Button
+                                        className="action-button ant-btn-danger"
+                                        onClick={() => confirmDeleteUser(user._id)}
+                                        icon={<RiDeleteBin6Line />}
+                                    >
+                                        Eliminar
+                                    </Button>
+                                    <Button className="action-button ant-btn-success">
+                                        Editar
+                                    </Button>
                                 </td>
                             </tr>
                         ))}
@@ -87,5 +118,3 @@ const UsersTable = () => {
 };
 
 export default UsersTable;
-
-
