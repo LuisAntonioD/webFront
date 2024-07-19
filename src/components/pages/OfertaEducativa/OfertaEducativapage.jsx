58c2348533a27262c6de9187667bdc2aba537e8d
AuraRenata812 <becaria.aura@mideservices.com>
2024-07-18 18:55:21 -0600

    nada modificado

--- src/components/pages/OfertaEducativa/OfertaEducativapage.jsx
+++ src/components/pages/OfertaEducativa/OfertaEducativapage.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
 import { Button, Layout, Menu, theme } from 'antd';
-import { LogoutOutlined, MenuFoldOutlined, MenuUnfoldOutlined, HomeOutlined, BookFilled, UserOutlined } from '@ant-design/icons';
+import { LogoutOutlined, MenuFoldOutlined, MenuUnfoldOutlined, HomeOutlined, BookFilled, FireFilled, UserOutlined } from '@ant-design/icons';
 import { useNavigate, Link } from "react-router-dom";
 import logo from '../../../assets/uteq3.png';
 import DrawerComponent from '../../Drawer';
 import ProfesoresTable from '../../OfertasEducativas/OfertasTabla/OfertasEducativasTable'; // Ajusta la importación según la estructura de tu proyecto
+// import NewProfesorForm from '../../../components/Profesores/ProfesoresTabla/Profesores/newProfesor'; // Importa el formulario NewProfesorForm
 import { useAuth } from '../../../hooks/useAuth'; // Asegúrate de importar useAuth si lo necesitas
+//import { profesoresService } from '../../../services/profesorService'; // Asegúrate de importar profesoresService si lo necesitas
 
 const { Header, Sider, Content, Footer } = Layout;
 
@@ -14,7 +16,7 @@ const OfertaEducativaPage = () => {
     const [collapsed, setCollapsed] = useState(false); // Define collapsed como estado local
     const { colorBgContainer } = theme.useToken().token;
     const navigate = useNavigate();
-    const [drawerVisible, setDrawerVisible] = useState(false); // Estado para controlar la visibilidad del drawer
+    const [modalVisible, setModalVisible] = useState(false); // Estado para controlar la visibilidad del modal
 
     const handleLogoClick = () => {
         setSelectedKey('');
@@ -28,6 +30,9 @@ const OfertaEducativaPage = () => {
             case '2':
                 navigate('/Admisiones');
                 break;
+            case '3':
+                navigate('/servicios');
+                break;
             case '4':
                 navigate('/Usuarios');
                 break;
@@ -38,15 +43,19 @@ const OfertaEducativaPage = () => {
                 navigate('/OfertaEducativa');
                 break;
             case '7':
-                setDrawerVisible(true); // Abre el drawer
+                <DrawerComponent />
                 break;
             default:
                 break;
         }
     };
 
-    const handleDrawerClose = () => {
-        setDrawerVisible(false); // Cierra el drawer
+    const handleAddProfesor = () => {
+        setModalVisible(true); // Abre el modal para agregar un nuevo profesor
+    };
+
+    const handleCancelAddOfertasEducativas = () => {
+        setModalVisible(false); // Cierra el modal para agregar un nuevo profesor
     };
 
     return (
@@ -67,7 +76,16 @@ const OfertaEducativaPage = () => {
                             { key: '4', icon: <UserOutlined />, label: 'Usuarios' },
                             { key: '5', icon: <UserOutlined />, label: 'Profesores' }, 
                             { key: '6', icon: <UserOutlined />, label: 'OfertaEducativa' }, 
-                            { key: '7', icon: <UserOutlined />, label: `${user.username}` }, // Usa un ícono adecuado
+
+                        ]}
+                    />
+                    <div style={{ flex: 1 }}></div>
+                    <Menu
+                        theme="dark"
+                        mode="inline"
+                        onClick={({ key }) => handleMenuClick(key)}
+                        items={[
+                            { key: '7', icon: <DrawerComponent />, label: `${user.username}` },
                         ]}
                     />
                     <div className="spacer" />
@@ -92,6 +110,17 @@ const OfertaEducativaPage = () => {
                 </Header>
                 <Content style={{ overflowX: 'auto', overflowY: 'auto', maxHeight: 'calc(100vh - 64px)' }}>
                     <div className="profesores-page-container">
+                        {/* <Button type="primary" onClick={handleAddProfesor} style={{ marginBottom: '16px' }}>
+                            Agregar Nuevo Profesor
+                        </Button> */}
+                        {/* <NewProfesorForm
+                            visible={modalVisible}
+                            onCreate={() => {
+                                // Implementa la lógica para actualizar los datos si es necesario
+                                setModalVisible(false); // Cierra el modal después de agregar el profesor
+                            }}
+                            onCancel={handleCancelAddProfesor}
+                        /> */}
                         <ProfesoresTable /> {/* Muestra la tabla de profesores */}
                     </div>
                     <Footer style={{ textAlign: 'center' }}>
@@ -101,6 +130,7 @@ const OfertaEducativaPage = () => {
                             <Link to="/servicios" style={{ margin: '0 10px', color: 'black' }}>Servicios</Link>
                             <Link to="/Usuarios" style={{ margin: '0 10px', color: 'black' }}>Usuarios</Link>
                             <Link to="/Profesores" style={{ margin: '0 10px', color: 'black' }}>Profesores</Link>
+
                         </div>
                         <div style={{ marginTop: '20px' }}>
                             Uteq 2.0 ©2024 Created by TeamAura
@@ -108,7 +138,6 @@ const OfertaEducativaPage = () => {
                     </Footer>
                 </Content>
             </Layout>
-            <DrawerComponent visible={drawerVisible} onClose={handleDrawerClose} /> {/* Añade el drawer */}
         </Layout>
     );
 };
