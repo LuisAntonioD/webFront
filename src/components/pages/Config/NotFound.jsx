8c81d1480711493e8571a303ecc4e50d0cc45a90
CARLOS MANUEL ESPINOZA MENDOZA <2020371019@gmail.com>
2024-07-18 18:38:59 -0600

    (feat):Se_actualizo_menu_codigo_mas_limpio

--- src/components/pages/Config/NotFound.jsx
+++ src/components/pages/Config/NotFound.jsx
@@ -1,97 +1,34 @@
-// src/pages/NotFound.jsx
 import React, { useState } from 'react';
 import './NotFound.css';
 import { useAuth } from '../../../hooks/useAuth';
-import { Button, Layout, Menu, theme } from 'antd';
-import { LogoutOutlined, 
-    MenuFoldOutlined, 
-  MenuUnfoldOutlined,
-  HomeOutlined,
-  BookFilled,
-  FireFilled,
-  UserOutlined
-    } from '@ant-design/icons'; // Importamos el icono de logout
-import { useNavigate, Link } from "react-router-dom";
-import logo from '../../../assets/uteq3.png';
-import DrawerComponent from '../../Drawer';
+import { Button, Layout, theme } from 'antd';
+import { MenuFoldOutlined, MenuUnfoldOutlined } from '@ant-design/icons';
+import SidebarMenu from '../Menu/SidebarMenu';
+import { useMenuConfig } from '../Menu/HandleMenu';
+import FooterNav  from '../Menu/Footer.jsx';
 
-const { Header, Sider, Content, Footer } = Layout;
+const { Header, Content } = Layout;
 
 const NotFound = () => {
 
     const { user, logout } = useAuth();
     const [collapsed, setCollapsed] = useState(false);
   const { colorBgContainer, borderRadiusLG } = theme.useToken().token;
-  const navigate = useNavigate();
-
-  const handleLogoClick = () => {
-    setSelectedKey('');
-  };
-
-  const handleMenuClick = (key) => {
-    switch (key) {
-      case '1':
-        navigate('/');
-        break;
-      case '2':
-        navigate('/Admisiones');
-        break;
-      case '3':
-        navigate('/servicios');
-        break;
-        case '4':
-          navigate('/Usuarios');
-        break;
-        case '5':
-          navigate('/Profesores');
-        break;
-        case '6':
-          <DrawerComponent/>
-        break;
-        
-      default:
-        break;
-    }
-  };
-
+  const handleMenuClick = useMenuConfig();
+  
     return (
-        <Layout style={{ minHeight: '100vh' }}>
-      <Sider trigger={null} collapsible collapsed={collapsed}>
-        <Link to="/" className="logo" onClick={handleLogoClick}>
-          <img src={logo} alt="logo" />
-        </Link>
-        <Menu
-          theme="dark"
-          mode="inline"
-          defaultSelectedKeys={['1']}
-          onClick={({ key }) => handleMenuClick(key)}
-          items={[
-            { key: '1', icon: <HomeOutlined />, label: 'Inicio' },
-            { key: '2', icon: <BookFilled />, label: 'Admisiones' },
-            { key: '3', icon: <FireFilled />, label: 'Servicios' },
-            { key: '4', icon: <UserOutlined />, label: 'Usuarios' },
-            { key: '5', icon: <UserOutlined />, label: 'Profesores' },
-          ]}
-        />
-        <div style={{ flex: 1 }}></div>
-        <Menu
-          theme="dark"
-          mode="inline"
-          onClick={({ key }) => handleMenuClick(key)}
-          items={[
-            { key: '6', icon: <DrawerComponent/>, label: `${user.username}` },
-          ]}
-        />
-        <div className="spacer" />
-        <Button
-          className="logout-button"
-          onClick={() => logout()}
-          icon={<LogoutOutlined />}
-          style={{ width: '100%', textAlign: 'left', paddingLeft: '24px', paddingTop: '12px', paddingBottom: '12px' }}
-        >
-          Logout
-        </Button>
-      </Sider>
+      <Layout style={{ minHeight: '100vh' }}>
+      {user && (
+          <>
+              <SidebarMenu 
+                  user={user} 
+                  collapsed={collapsed} 
+                  setCollapsed={setCollapsed} 
+                  handleMenuClick={handleMenuClick} 
+                  logout={logout} 
+              />
+          </>
+      )}
       <Layout>
         <Header style={{ padding: 0, background: colorBgContainer }}>
           <Button
@@ -111,18 +48,7 @@ const NotFound = () => {
             <h1>Desarrollo</h1>
             <h2>La pagina es inexistente  :c</h2>
             <p>Próximamente</p>
-            <Footer style={{ textAlign: 'center', marginTop:400 }}>
-              <div>
-              <Link to="/" style={{ margin: '0 10px', color: 'black' }}>Inicio</Link>
-                            <Link to="/Admisiones" style={{ margin: '0 10px', color: 'black' }}>Admisiones</Link>
-                            <Link to="/servicios" style={{ margin: '0 10px', color: 'black' }}>Servicios</Link>
-                            <Link to="/Usuarios" style={{ margin: '0 10px', color: 'black' }}>Usuarios</Link>
-                            <Link to="/Profesores" style={{ margin: '0 10px', color: 'black' }}>Profesores</Link>
-              </div>
-              <div style={{ marginTop: '20px' }}>
-                  Uteq 2.0 ©2024 Created by TeamAura
-              </div>
-          </Footer>
+            <FooterNav/>
         </div>
         
         </Content>
---
+++ src/components/pages/Menu/Footer.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Layout } from 'antd';
+import { Link } from 'react-router-dom';
+
+const { Footer } = Layout;
+
+const FooterNav = () => {
+    return (
+        <Footer style={{ textAlign: 'center' }}>
+            <div>
+                <Link to="/" style={{ margin: '0 10px', color: 'black' }}>Inicio</Link>
+                <Link to="/Admisiones" style={{ margin: '0 10px', color: 'black' }}>Admisiones</Link>
+                <Link to="/servicios" style={{ margin: '0 10px', color: 'black' }}>Servicios</Link>
+                <Link to="/Usuarios" style={{ margin: '0 10px', color: 'black' }}>Usuarios</Link>
+                <Link to="/Profesores" style={{ margin: '0 10px', color: 'black' }}>Profesores</Link>
+            </div>
+            <div style={{ marginTop: '20px' }}>
+                Uteq 2.0 ©2024 Created by TeamAura
+            </div>
+        </Footer>
+    );
+};
+
+export default FooterNav;
