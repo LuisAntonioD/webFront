f589cfee5daa8316674f6a302bdcf637ac72b72d
LuisAntonioD <luisdeanda320@gmail.com>
2024-09-16 22:52:40 -0600

    Ejemplo

--- src/components/ImageLogin/index.jsx
+++ src/components/ImageLogin/index.jsx
@@ -6,19 +6,7 @@ import './ImageLogin.css'; // Archivo CSS para estilos específicos si es necesa
 const ImageLogin = () => {
   return (
     <div>
-       {/*
-      <img src={loginImage} alt="Login" />
-      Agrega un enlace a la página de productos */}
-       {/*
-      <div className="button-container">
-        <Link to="/mostraradmisiones">
-          <button className="ver-productos-btn">Nuestras Admisiones</button>
-        </Link>
-        <Link to="/mostrarofertas">
-          <button className="ver-productos-btn">Nuestras Ofertas</button>
-        </Link>
-      </div>
-              */}
+      
 
     </div>
   );
